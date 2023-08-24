46f93247e31754f95fccce8c65efa6d36cb29c5a
sudheer0071 <sudheer1614@gmail.com>
2023-08-24 22:55:11 +0530

    default changes

--- mlhybridx/default.py
+++ mlhybridx/default.py
@@ -8,13 +8,15 @@ from .train import train_data
 
 
 def multiple_data():
-   flight = sns.load_dataset('flights').replace({'Jan': 1, 'Feb':2,'Mar':3,'Apr':4,'May':5,'Jun':6,'Jul':7,'Aug':8,'Sep':9,'Oct':10,'Nov':11,'Dec':12}) 
-  
-   tips = sns.load_dataset('tips')
-   car_crash = sns.load_dataset('car_crashes').drop(['abbrev'], axis=1)
-   datasets = [flight, car_crash, tips]
-   data = random.choice(datasets)
-   return data
+        flight = sns.load_dataset('flights').replace({'Jan': 1, 'Feb':2,'Mar':3,'Apr':4,'May':5,'Jun':6,'Jul':7,'Aug':8,'Sep':9,'Oct':10,'Nov':11,'Dec':12}) 
+        anscombe = sns.load_dataset('anscombe').replace({'I':1,'II':2,'III':3,'IV':4})
+        car_crash = sns.load_dataset('car_crashes').drop(['abbrev'], axis=1)
+        dowjones = sns.load_dataset('dowjones')
+        seaice = sns.load_dataset('seaice')
+        datasets = [flight, anscombe, car_crash, dowjones, seaice]
+        df = random.choice(datasets)
+        data = pd.DataFrame(df)
+        return data
 
 
         
