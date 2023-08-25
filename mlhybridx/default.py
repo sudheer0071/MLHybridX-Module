5c97ab3e78b80ea8ecdc3195ab7b331676b1d14c
sudheer0071 <sudheer1614@gmail.com>
2023-08-25 21:57:42 +0530

    chaning in default method

--- mlhybridx/default.py
+++ mlhybridx/default.py
@@ -20,6 +20,53 @@ def datasets():
         return name, data
 
 
+def By_default():
+
+        name, data = datasets()
+        print(f"*///////////////////////////////////////// ●▬▬▬▬◤ {name} Dataset ◢▬▬▬▬● ///////////////////////////////////////////\n\n")
+        
+        print(data)
+        target=""
+
+        print("\n\n\n\n\n\n\n\n//////////////////////////////// ●▬▬▬▬◤ split data ◢▬▬▬▬● //////////////////////////////////////\n\n")
+        x,y = split_data(data, target)
+        print(f"X: {x}\n\n\n y: {y}")
+
+        print("\n\n\n\n\n\n\n\n\n//////////////////////////////////// ●▬▬▬▬◤ trained data ◢▬▬▬▬● ///////////////////////////////\n\n")
+        x,  y = split_data(data, target)
+        size = 0.2
+        x_train, x_test, y_train, y_test = train_data( x,  y, size)
+        print(f"X_train: {x_train}\n\n\n y_train: {y_train}\n\n\nX_test: {x_test}\n\n\n y_test: {y_test}")
+
+        print("\n\n\n\n\n///////////////////////////////////////// ●▬▬▬▬◤ fitting into Liner Regression Models ◢▬▬▬▬● ////////////////////////////////////////")
+        
+
+        print("\n\n\n////////////////////////////////////////////////// ●▬▬▬▬◤ By ols ◢▬▬▬▬● /////////////////////////////////////////////////\n")
+        x_train,  x_test,  y_train,  y_test = train_data( x,  y , size)
+        m,b = ols( x_train,  y_train)
+        print(f"m = {m}\nb = {b} ")
+        pred = predict_gdr(x_test,m,b)
+        print(f" Predicted value = {pred}")
+
+        
+        print("\n\n\n//////////////////////////////////////////// ●▬▬▬▬◤ By Multiple Regression ◢▬▬▬▬● ///////////////////////////////////////\n")
+        x_train,  x_test,  y_train,  y_test = train_data( x,  y , size)
+        intercept_, coef_ = multiple( x_train,  y_train)
+        print(f"intercept_ = {intercept_}\ncoef_ = {coef_}")
+        pred = predict_gdr(x_test,m,b)
+        print(f" Predicted value = {pred}")
+
+
+        print("\n\n\n///////////////////////////////////// ●▬▬▬▬◤ By Gradient Descent Regression ◢▬▬▬▬● //////////////////////////////////////\n")
+        print("\n\n\n For lr = 0.001 and epochs = 50\n\n")
+        x_train,  x_test,  y_train,  y_test = train_data( x,  y , size)
+        intercept_, coef_= GDR(x_train, y_train, lr=0.001, epochs=50)
+        print(f"intercept = {intercept_}\ncoefficient = {coef_}")
+        pred = predict_gdr(x_test,m,b)
+        
+        print(f" Predicted value = {pred}")
+        
+
         
 
 
