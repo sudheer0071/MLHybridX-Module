b2c98e28bc91da363135c50a84f790b4103f9a74
sudheer0071 <sudheer1614@gmail.com>
2023-08-25 21:54:55 +0530

    .

--- mlhybridx/check.py
+++ mlhybridx/check.py
@@ -66,54 +66,8 @@ class EasyRegressor:
         print(f" //////////////////////////////// ●▬▬▬▬◤ x_train ◢▬▬▬▬● //////////////////////////////\n\n{self.x_train}\n\nshape of x_train = {self.x_train.shape}\n\n\n///////////////////////////////// ●▬▬▬▬◤ y_train ◢▬▬▬▬● /////////////////////////////\n\n{self.y_train}\n\nshape of y_train = {self.y_train.shape}\n\n\n ///////////////////////////////// ●▬▬▬▬◤ x_test ◢▬▬▬▬● //////////////////////////////\n\n{self.x_test}\n\nshape of x_test = {self.x_test.shape}\n\n\n ///////////////////////////////// ●▬▬▬▬◤ y_test ◢▬▬▬▬● //////////////////////////////\n\n{self.y_test}\n\nshape of y_test = {self.y_test.shape}")
         # return self.type(output, 0.000000001)
         return None
+          
          
-    def default(self):
-        data = multiple_data()
-        print("*///////////////////////////////////////// ●▬▬▬▬◤ Dataset ◢▬▬▬▬● ///////////////////////////////////////////\n\n")
-        print(data)
-
-        target=""
-
-        print("\n\n\n\n\n\n\n\n//////////////////////////////// ●▬▬▬▬◤ split data ◢▬▬▬▬● //////////////////////////////////////\n\n")
-        x,y = split_data(data, target)
-        print(f"X: {x}\n\n\n y: {y}")
-
-        print("\n\n\n\n\n\n\n\n\n//////////////////////////////////// ●▬▬▬▬◤ trained data ◢▬▬▬▬● ///////////////////////////////\n\n")
-        x,  y = split_data(data, target)
-        size = 0.2
-        x_train, x_test, y_train, y_test = train_data( x,  y, size)
-        print(f"X_train: {x_train}\n\n\n y_train: {y_train}\n\n\nX_test: {x_test}\n\n\n y_test: {y_test}")
-
-        print("\n\n\n\n\n///////////////////////////////////////// ●▬▬▬▬◤ fitting into Liner Regression Models ◢▬▬▬▬● ////////////////////////////////////////")
-        
-
-        print("\n\n\n////////////////////////////////////////////////// ●▬▬▬▬◤ By ols ◢▬▬▬▬● /////////////////////////////////////////////////\n")
-        x_train,  x_test,  y_train,  y_test = train_data( x,  y , size)
-        m,b = ols( x_train,  y_train)
-        print(f"m = {m}\nb = {b} ")
-        pred = predict_gdr(x_test,m,b)
-        print(f" Predicted value = {pred}")
-
-        
-        print("\n\n\n//////////////////////////////////////////// ●▬▬▬▬◤ By Multiple Regression ◢▬▬▬▬● ///////////////////////////////////////\n")
-        x_train,  x_test,  y_train,  y_test = train_data( x,  y , size)
-        intercept_, coef_ = multiple( x_train,  y_train)
-        print(f"intercept_ = {intercept_}\ncoef_ = {coef_}")
-        pred = predict_gdr(x_test,m,b)
-        print(f" Predicted value = {pred}")
-
-
-        print("\n\n\n///////////////////////////////////// ●▬▬▬▬◤ By Gradient Descent Regression ◢▬▬▬▬● //////////////////////////////////////\n")
-        print("\n\n\n For lr = 0.001 and epochs = 50\n\n")
-        x_train,  x_test,  y_train,  y_test = train_data( x,  y , size)
-        intercept_, coef_= GDR(x_train, y_train, lr=0.001, epochs=50)
-        print(f"intercept = {intercept_}\ncoefficient = {coef_}")
-        pred = predict_gdr(x_test,m,b)
-        
-        print(f" Predicted value = {pred}")
-
-        return None
-
     def model_ols(self):
         self.df()
         x, y  = split_data(self.data, self.target)
