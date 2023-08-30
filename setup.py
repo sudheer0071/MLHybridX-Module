b69712671e826c9367d9ea2375809e7df03d0b83
sudheer0071 <sudheer1614@gmail.com>
2023-08-30 16:27:47 +0530

    changes in setup file

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import setup, find_packages
 with open('README.md', 'r',encoding='utf-8') as f:
     LONG_DESCRIPTION = f.read()
 
-VERSION = '1.1.0'
+VERSION = '1.2.0'
 DESCRIPTION = 'ML module'
 
 setup(
